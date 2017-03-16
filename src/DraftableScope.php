a075f90d256d1b9d313f91ecfa9364a0c6d958c1
Chris Bratherton <chris@britishsoftware.co>
2017-03-16 12:32:00 +0000

    Updated namespace

--- src/DraftableScope.php
+++ src/DraftableScope.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Scope;
 
-class DraftableScope implements ScopeInterface {
+class DraftableScope implements Scope{
 
     /**
      * Apply scope on the query.
